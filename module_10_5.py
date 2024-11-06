102e1f3e95862609fc7cded54fd033d496787754
TheFUlDeep <39480610+TheFUlDeep@users.noreply.github.com>
2024-11-06 09:29:00 +0300

    Create module_10_5.py

---
+++ module_10_5.py
@@ -0,0 +1,38 @@
+import time, datetime, multiprocessing
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r', encoding='utf-8') as file:
+        for line in file:
+            if line == '':
+                break
+            all_data.append(line)
+
+
+def get_files():
+    yield from ("file " + str(x) + '.txt' for x in range(1, 5))
+
+
+def main():
+    started_at = time.time()
+    for file in get_files():
+        read_info(file)
+    print(f'{datetime.timedelta(seconds=time.time() - started_at)} (линейный)')
+
+    processes = []
+    for file in get_files():
+        process = multiprocessing.Process(target=read_info, args=(file,))
+        processes.append(process)
+
+    started_at = time.time()
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+    print(f'{datetime.timedelta(seconds=time.time() - started_at)} (многопроцессорный)')
+
+
+if __name__ == "__main__":
+    main()
