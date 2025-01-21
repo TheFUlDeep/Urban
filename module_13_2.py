0514853850da4fb2f2df6e50cc23014b6ba5692a
TheFUlDeep <39480610+TheFUlDeep@users.noreply.github.com>
2025-01-21 20:42:56 +0400

    Create module_13_2.py

---
+++ module_13_2.py
@@ -0,0 +1,28 @@
+# executor нет в новой версии
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+# aiogram.contrib нет в новой версии и fsm_storage
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(storage=MemoryStorage())
+
+
+@dp.message(CommandStart())
+async def start(msg):
+    print('Привет! Я бот помогающий твоему здоровью')
+
+
+@dp.message()
+async def start(msg):
+    print('Введите команду /start, чтобы начать общение')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
